516786fe53eac800760db4ff66874bbe583326b5
Myron <myook@appacademy.io>
2022-08-22 15:51:43 -0400

    initialized and added store

--- w12d2 benchbnb/frontend/bench_bnb.jsx
+++ w12d2 benchbnb/frontend/bench_bnb.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore()
+  // Testing
+  window.getState = store.getState
+  window.dispatch = store.dispatch
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
