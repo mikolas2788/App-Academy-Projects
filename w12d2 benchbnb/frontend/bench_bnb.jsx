bfe822d10ecfe81cdc5573200a7b9cd80724ffbe
Myron <myook@appacademy.io>
2022-08-22 17:44:28 -0400

    refactored working store to app and root

--- w12d2 benchbnb/frontend/bench_bnb.jsx
+++ w12d2 benchbnb/frontend/bench_bnb.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
+import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore()
@@ -8,5 +9,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.getState = store.getState
   window.dispatch = store.dispatch
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
