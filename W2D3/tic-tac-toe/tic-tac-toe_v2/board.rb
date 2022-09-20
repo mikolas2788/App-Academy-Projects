3cb7c41861fad3e7dc0c9c0557ec0a485b10ddf6
Myron <myook@appacademy.io>
2022-09-19 22:12:02 -0400

    work to tic-tac-toe_v2

---
+++ W2D3/tic-tac-toe/tic-tac-toe_v2/board.rb
@@ -0,0 +1,83 @@
+require "byebug"
+
+class Board
+
+    attr_reader :grid
+
+    def initialize(board_length)
+        @grid = Array.new(board_length) { Array.new(board_length, "_") }
+    end 
+
+    def [](pos)
+        row, col = pos
+        debugger
+        @grid[row][col]
+    end 
+
+    def []=(pos, mark)
+        row, col = pos
+        @grid[row][col] = mark
+    end 
+
+    def valid?(pos)
+        pos.none?(&:negative?) && pos.none? { |num| num > @grid.length - 1}
+    end 
+
+    def empty?(pos)
+        self[pos] == "_"
+    end 
+
+    def place_mark(pos, mark)
+        if self.valid?(pos) && self.empty?(pos)
+            self[pos] = mark
+        elsif !self.valid?(pos)
+            raise "Not a valid position"
+        elsif !self.empty?(pos)
+            raise "That position is already taken"
+        end 
+    end
+
+    def print
+        grid.each { |row| p row.join(" ") }
+    end 
+
+    def win_row?(mark)
+        grid.any? do |row|
+            row.all? { |sector| sector == mark }
+        end 
+    end 
+
+    def win_col?(mark)
+        grid.transpose.any? do |column|
+            column.all? { |sector| sector == mark }
+        end 
+    end 
+
+    def win_diagonal?(mark)
+        left_diagonal = (0...grid.length).all? do |i|
+            pos = [i, i]
+            self[pos] == mark
+        end 
+
+        right_diagonal = (0...grid.length).all? do |i|
+            row = i
+            col = grid.length - 1 - i
+            pos = [row, col]
+            self[pos] == mark
+        end 
+
+        left_diagonal || right_diagonal
+    end
+
+    def win?(mark)
+        win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+    end 
+
+    def empty_positions?
+        grid.any? do |row|
+            row.any? do |col|
+                col == "_"
+            end 
+        end 
+    end 
+end 
